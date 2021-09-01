3d2976fcd0a164da03642dd9b58902d1e4e49daa
Willian Douglas <willalex2012@gmail.com>
2021-09-01 20:24:33 -0300

    Atividade do Encontro Remoto 2

---
+++ sistema.js
@@ -0,0 +1,56 @@
+let numeroDeAlunos = 10;
+
+let contador = 0;
+
+   if(contador === 0){
+       console.log("O número atual é ZERO")
+    } else if (contador % 2 == 1 ) {
+       console.log("O numero " + contador + " é IMPAR")
+  }else {
+       console.log("O numero " + contador + "é PAR")
+    }
+
+   contador++;
+while ( contador <= numeroDeAlunos) ;
+
+while ( contador <= numeroDeAlunos)
+
+if (contador === 0) {
+   console.log("Onumero atual é ZERO");
+} else if (contador % 2 == 1) {
+    console.log("O número " + contador + " é PAR e maior que 5")
+}else if (contador % 2 == 0) {
+    console.log("O numero " + contador + " é IMPAR")
+} else{
+   console.log("O númeor " + contador + " é PAR")
+}
+
+contador ++;
+
+deixa (numeroDeAlunos = 10);
+
+for (contador = 1; conatdor <= numeroDeAlunos ; contador++ ) {
+   //console.log (contador)
+
+   if ( contador === 0){
+       console.log("O numero atual é ZERO");
+   } else if ( contador % 2 == 0 && contador > 5 ){
+       console.log("O numero" + conatdor + " é PAR e maior que 5")
+   } else {
+       console.log( " Onúmero" + conatdor + " é IMPAR" )
+   }
+
+if (!usuario) {
+    console.log("Aresposta está vazia")
+}
+
+let listaDeAlunos = ["Marcelo", "Juh", "Cleber", "Wesley", "Adriano"]
+
+for ( const aluno of listaDeAlunos) {
+     console.log(aluno);
+}
+
+listaDeAluno.forEach(element=> {
+      console.log(aluno)
+} ) ;
+}
